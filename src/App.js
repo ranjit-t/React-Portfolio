006bf00a2acc159ea3d9f2588adda21a9dd3525a
Ranjit <amailtoranjith@gmail.com>
2023-05-11 14:37:58 +0200

    logo animation

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Contact from "./Pages/Contact";
 import Landing from "./Pages/Landing";
 import MySkills from "./Pages/MySkills";
 import Projects from "./Pages/Projects";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import PageViewer from "./Components/PageViewer";
 import Logo from "./Images/rtwhite.png";
@@ -14,12 +14,39 @@ import Linkedin from "./Images/linkedin.png";
 function App() {
   const windowSize = useRef([window.innerWidth, window.innerHeight]);
   const [selectedPage, setSelectedPage] = useState("landing");
-  return (
-    <div className="App ">
-      {/* <p className="logo">
-        <b>RT</b>
-      </p> */}
+  const [pageLoading, setPageLoading] = useState(true);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setPageLoading(false);
+    }, 2000);
+  }, []);
+
+  const containerVariants = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1, transition: { duration: 1 } },
+    exit: { opacity: 0, transition: { duration: 0.5 } },
+  };
+
+  const logoVariants = {
+    initial: { rotate: 0 },
+    animate: { rotate: 360, transition: { duration: 1, repeat: Infinity } },
+  };
+
+  return pageLoading ? (
+    <motion.div
+      className="loader"
+      variants={containerVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+    >
+      <motion.div variants={logoVariants}>
+        <img src={Logo} alt="logo" width="180px" />
+      </motion.div>
+    </motion.div>
+  ) : (
+    <div className="App ">
       <div className="logo">
         <img src={Logo} alt="logo" width="80px" />
       </div>
