8ef3dfd45e524637db004c6ecdf31dc942f0fd78
Ranjit <amailtoranjith@gmail.com>
2023-05-11 17:46:03 +0200

    skills image margin

--- src/Pages/MySkills.js
+++ src/Pages/MySkills.js
@@ -35,7 +35,7 @@ export default function MySkills() {
       </div>
       <div className="skills-photo">
         <div
-          className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
+          className="relative z-0 ml-20 before:absolute before:-top-8 before:-left-8
               before:w-full before:h-full before:border-2 before:border-blue before:z-[-1] "
         >
           <img
